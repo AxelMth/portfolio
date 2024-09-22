bc4653827233c7beef6da8e396def0bfd79bd22e
AxelMth <aujoulator@hotmail.fr>
2024-09-22 11:57:19 +0200

    Add applications and formation components

---
+++ src/components/Applications.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import ApplicationItem from './ApplicationItem.vue';
+const applications = [
+    {
+        name: 'Earthunt',
+        description: 'An exciting game available on iOS.',
+        logo: './assets/earthunt.png',
+        siteUrl: 'https://marcel.games'
+    },
+    {
+        name: 'Verdict',
+        description: 'An app to create quizz fully configurable. Ask for a demo.',
+        logo: './assets/twit-hair.png',
+        siteUrl: 'https://verdict.axelmathi.eu'
+    },
+    {
+        name: 'Twit\'hair',
+        description: 'A Twitter bot that tweets about hair saloons with a pun.',
+        logo: './assets/twit-hair.png',
+        siteUrl: 'https://twitter.com/twitt_hair_'
+    }
+]
+</script>
+<template>
+    <div v-for="application in applications" :key="application.name">
+        <ApplicationItem :name="application.name" :description="application.description" :siteUrl="application.siteUrl" :logo="application.logo" />
+    </div>
+</template>
---
+++ src/components/FormationItem.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+defineProps<{ title: string; description: string; startDate: string; endDate: string }>();
+</script>
+<template>
+    <article>
+        <header>
+            <h2>{{ title }}</h2>
+            <p>{{ startDate }} - {{ endDate }}</p>
+        </header>
+        <p>{{ description }}</p>
+        <span>{{  new Date(startDate) }} - {{ new Date(endDate) }}</span>
+    </article>
+</template>
