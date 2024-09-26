7a119a3065011c72d5906d2051a8248c227e99d8
AxelMth <aujoulator@hotmail.fr>
2024-09-26 21:19:27 +0200

    add terminal page

---
+++ src/Terminal.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const text = ref<string>('');
+const logs = ref<string[]>([]);
+const inputRef = ref<HTMLInputElement | null>(null);
+
+const commands: Record<string, () => void> = {
+  help: () => {
+    logs.value.push('Available commands:');
+    logs.value.push('about - About this terminal');
+    logs.value.push('contact - Contact information');
+  },
+  about: () => {
+    logs.value.push('This is a simple terminal created with Vue 3.');
+  },
+  clear: () => {
+    logs.value = [];
+  },
+  contact: () => {
+    logs.value.push('Email: some-email@gmail.com');
+  }
+}
+
+const onEnter = () => {
+  if (commands[text.value]) {
+    commands[text.value]();
+  } else {
+    logs.value.push(`Command not found: ${text.value}`);
+  }
+  logs.value = [...logs.value];
+
+  text.value = '';
+}
+
+onMounted(() => {
+  if (inputRef.value) {
+    inputRef.value.focus();
+  }
+  logs.value.push('Welcome to my terminal! Type help to see the available commands.');
+});
+</script>
+
+<template>
+  <main>
+    <p v-for="log in logs" :key="log">{{ log }}</p>
+    <p>
+      admin> <input type="text" v-model="text" @keyup.enter="onEnter" ref="inputRef"/>
+    </p>
+  </main>
+</template>
+
+<style scoped>
+  input {
+    background-color: black;
+    color: white;
+    border: none;
+    outline: none;
+  }
+</style>
