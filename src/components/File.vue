61d0dd4137d37b71ab8f03cb2f1d506fcf2bf8b0
AxelMth <aujoulator@hotmail.fr>
2024-09-28 14:56:17 +0200

    apply hover style to element

--- src/components/File.vue
+++ src/components/File.vue
@@ -16,10 +16,26 @@ defineProps<{ name: string; iconUrl: string }>();
     align-self: center;
     padding: 15px;
   }
+  img:hover {
+    cursor: pointer;
+  }
+  img:hover + p {
+    color: #fff;
+    cursor: pointer;
+
+    background: #9d0ffd;
+    background: url(../img/bk.png), -moz-linear-gradient(left, #9d0ffd 0%, #e60efe 100%);
+    background: url(../img/bk.png), -webkit-linear-gradient(left, #9d0ffd 0%, #e60efe 100%);
+    background: url(../img/bk.png), linear-gradient(to right, #9d0ffd 0%, #e60efe 100%);
+  }
   p {
     font-weight: bold;
     font-size: medium;
     color: black;
     text-align: center;
+  }
+  p:hover {
+    color: black;
+    cursor: pointer;
   }
 </style>
